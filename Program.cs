a850e93a1aee589051d2511fd1852d57b4efa213
yjywybb <yjywybb@hotmail.com>
2021-05-09 16:07:46 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace FFXIV_OTP_Assistant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long duration = 30;
+            string key = Encoding.Default.GetString(Base32.ToBytes(args[0]));
+            GoogleAuthenticator authenticator = new GoogleAuthenticator(duration, key);
+            var code = authenticator.GenerateCode();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://127.0.0.1:4646/ffxivlauncher/" + code);
+            request.GetResponse();
+        }
+    }
+}
